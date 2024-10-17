181b2d57cc1ee43546aed60fdc5e03fa9d134387
unknown <simona.stoykova@orbisagency.com>
2024-10-17 21:46:56 +0300

    hero fixes desktop

--- wp-content/themes/sima-theme/components/modules/module_hero.php
+++ wp-content/themes/sima-theme/components/modules/module_hero.php
@@ -19,25 +19,32 @@ $hero_image = (isset($module['hero_image'])) ? $module['hero_image'] : false;
                     <div class="slide">
                         <div class="content">
                             <?php if ($has_text) { ?>
-                                <div class="slide__text standard-grid">
-                                    <?php if (isset($slide['slide_title']) && !empty($slide['slide_title'])) { ?>
-                                        <div class="slide__title">
-                                            <h1><?php echo $slide['slide_title']; ?></h1>
+                                <div class="slide__text">
+                                    <div class="descr standard-grid">
+                                        <?php if (isset($slide['slide_title']) && !empty($slide['slide_title'])) { ?>
+                                            <div class="slide__title">
+                                                <h1><?php echo $slide['slide_title']; ?></h1>
+                                            </div>
+                                        <?php } ?>
+                                        <?php if (isset($slide['slide_description']) && !empty($slide['slide_description'])) { ?>
+                                            <div class="slide__description"><?php echo $slide['slide_description']; ?></div>
+                                        <?php } ?>
+                                    </div>
+                                    <?php if (isset($slide['slide_link']['url']) && !empty($slide['slide_link']['url'])) { ?>
+                                        <div class="slide__button">
+                                            <a href="<?php echo $slide['slide_link']['url']; ?>" class="main-button">
+                                                <?php echo $slide['slide_link']['title']; ?>
+                                            </a>
                                         </div>
-                                    <?php } ?>
-                                    <?php if (isset($slide['slide_description']) && !empty($slide['slide_description'])) { ?>
-                                        <div class="slide__description"><?php echo $slide['slide_description']; ?></div>
                                     <?php } ?>
                                 </div>
                             <?php } ?>
                             <?php if (isset($slide['slide_image']['url']) && !empty($slide['slide_image']['url'])) { 
                                 $alt = (isset($slide['slide_image']['alt']) && !empty($slide['slide_image']['alt'])) ? $slide['slide_image']['alt'] : __('Slide Image', 'sima-theme');
                                 ?>
-                                <a href="<?php echo (isset($slide['slide_link']['url']) && !empty($slide['slide_link']['url'])) ? $slide['slide_link']['url'] : "javascript:void(0)" ;?>">
-                                    <div class="slide__img">
-                                        <img src="<?php echo $slide['slide_image']['url']; ?>" alt="<?php echo $alt; ?>">
-                                    </div>
-                                </a>
+                                <div class="slide__img">
+                                    <img src="<?php echo $slide['slide_image']['url']; ?>" alt="<?php echo $alt; ?>">
+                                </div>
                             <?php } ?>
                         </div>
                     </div>
