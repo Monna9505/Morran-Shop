37279f6134cd20aa24852eb266e6862c2e8415e5
unknown <simona.stoykova@orbisagency.com>
2025-04-23 21:45:03 +0300

    video style bugs

--- wp-content/themes/sima-theme/assets/private/js/includes/animated-text.js
+++ wp-content/themes/sima-theme/assets/private/js/includes/animated-text.js
@@ -2,14 +2,20 @@ function adjustFontSize() {
     const width = $(window).width();
     const svgPositions = $('.video .content .animated__text svg');
     const tspan = $('.video .content .animated__text svg text tspan');
+    const logo = $('.video .content .animated__text svg image');
+    const text = $('.video .content .animated__text svg text');
 
     if (width <= 768) {
-        tspan.eq(0).attr('x', '1%');
+        tspan.eq(0).attr('x', '3%');
         tspan.eq(0).attr('dy', '0');
-        tspan.eq(1).attr('x', '1%');
-        tspan.eq(1).attr('dy', '150');
+        logo.attr('y', '0');
+        logo.attr('x', '0');
+        logo.attr('width', '400');
+        logo.attr('height', '520');
 
-        svgPositions.attr('viewBox', '9.5 50 960 350');
+        text.attr('font-size', '90');
+        text.attr('stroke-width', '3');
+        svgPositions.attr('viewBox', '9.5 0 960 350');
     } 
 }
     
@@ -23,7 +29,7 @@ $(document).ready(function () {
     function isInViewport(element) {
         let elementTop = $(element).offset().top;
         let elementBottom = elementTop + $(element).outerHeight();
-        let viewportTop = $(window).scrollTop();
+        let viewportTop = $(window).scrollTop() + 400;
         let viewportBottom = viewportTop + $(window).height();
 
         return elementBottom > viewportTop && elementTop < viewportBottom;
--- wp-content/themes/sima-theme/components/modules/module_video-component.php
+++ wp-content/themes/sima-theme/components/modules/module_video-component.php
@@ -26,12 +26,16 @@ $video = (isset($module['video'])) ? $module['video'] : false;
                                 <animate attributeName="stop-color" values="#EBAB12;#EBAB12;#EBAB12;#EBAB12" dur="4s" repeatCount="indefinite" />
                             </stop>
                         </defs>
-                        <text x="50%" y="50%" font-size="140" stroke-linecap="round" stroke-linejoin="round" 
-                            fill="none" stroke="#EBAB12" stroke-width="5" stroke-dasharray="1000" stroke-dashoffset="600">
-                            <tspan x="2%" dy="100"><?php echo __('Our work at', 'sima-theme'); ?></tspan>
-                            <tspan x="2%" dy="200"><?php echo __('Morran Studio', 'sima-theme'); ?></tspan>
-                            <animate id="strokeAnimation" attributeName="stroke-dashoffset" from="1000" to="0" dur="7s" fill="freeze" />
-                        </text>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 655">
+                            <text x="50%" y="50%" font-size="140" stroke-linecap="round" stroke-linejoin="round" 
+                                    fill="none" stroke="#000000" stroke-width="4" stroke-dasharray="1000" stroke-dashoffset="500">
+                                <tspan x="2%" dy="100"><?php echo __('Our work at', 'sima-theme'); ?></tspan>
+                                <animate id="strokeAnimation" attributeName="stroke-dashoffset" from="1300" to="0" dur="6s" fill="freeze" />
+                            </text>
+                            <image x="-4%" y="200" width="700" height="700" xlink:href="/wp-content/uploads/2024/09/Morran-Studio-Logo.png">
+                                <animate attributeName="opacity" from="0" to="1" dur="3s" fill="freeze" />
+                            </image>
+                        </svg>
                     </svg>
                 </div>
             </div>
