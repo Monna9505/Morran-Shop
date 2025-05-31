a016d65e15b601c286841f2996f97f527f80cd78
Simona <simona.d.stoykova@gmail.com>
2025-05-31 18:32:12 +0300

    header fix and style bugs

--- wp-content/themes/sima-theme/components/shared/header.php
+++ wp-content/themes/sima-theme/components/shared/header.php
@@ -8,13 +8,53 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
+    <?php
+        $header_logo = get_field('header_logo', 'option') ?: false;
+    ?>
 </head>
 <body <?php body_class(); ?>>
     <header>
         <div class="header__wrapper">
-            <div class="container">
-                <?php include(locate_template('components/shared/header-first-part.php')); ?>
+            <div class="container standard-grid">
+                <?php if (!empty($header_logo['url'])) { 
+                    $alt = (isset($header_logo['alt']) && !empty($header_logo['alt'])) ? $header_logo['alt'] : 'Morran Studio Logo';
+                    ?>
+                    <a href="/" class="header__logo" title="Morran Studio Logo">
+                        <img src="<?php echo $header_logo['url']; ?>" alt="<?php echo $alt; ?>" loading="lazy">
+                    </a>
+                <?php } ?>
                 <?php include(locate_template('components/shared/header-links.php')); ?>
+                <div class="search__favourites__cart standard-grid">
+                    <div class="dots__menu">
+                        <div class="first standard-grid">
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                        </div>
+                        <div class="second standard-grid">
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                        </div>
+                        <div class="third standard-grid">
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                        </div>
+                    </div>
+                    <?php include(locate_template('components/shared/mobile-header.php')); ?>
+                    <div class="desktop-searchbar">
+                        <?php include(locate_template('components/shared/search-bar.php')); ?>
+                    </div>
+                    <a href="<?php echo wc_get_cart_url(); ?>" class="cart" title="<?php _e( 'View your shopping cart' ); ?>">
+                        <span class="cart__icon">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </span>
+                        <span class="cart__items__amount">
+                            <?php echo WC()->cart->get_cart_contents_count(); ?>
+                        </span>
+                    </a>
+                </div>
             </div>
         </div>
     </header>
--- wp-content/themes/sima-theme/page.php
+++ wp-content/themes/sima-theme/page.php
@@ -3,8 +3,17 @@
 $pageid = get_the_ID();
 
 include(locate_template('components/shared/header.php'));
-
 // ACF modules
 include(locate_template('components/modules/_modules.php'));
-
-include(locate_template('components/shared/footer.php'));
+?>
+<div class="site-content">
+    <div class="container">
+        <?php if ( have_posts() ) {
+            while (have_posts()) {
+                the_post();
+                the_content();
+            }
+        } ?>
+    </div>
+</div>
+<?php include(locate_template('components/shared/footer.php'));
